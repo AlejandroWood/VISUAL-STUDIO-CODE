a28973d7c8b24982d8b7cf515ebb2e840c1ac566
AlejandroWood <awoorod250@gmail.com>
2026-01-19 09:20:49 +0100

    main

---
+++ PROGRAMACION/UNIDAD3/Random/idiot2.java
@@ -0,0 +1,20 @@
+package UNIDAD3.Random;
+
+public class idiot2 {
+    public static void main(String[] args) {
+        boolean siempre = true;
+
+        while (siempre) {
+            System.out.println("YOU ARE AN IDIOT                                                                                                                                                                               ");
+            System.out.println("                                            YOU ARE AN IDIOT                                                                                                                                   ");
+            System.out.println("                                                                                        YOU ARE AN IDIOT                                                                                       ");
+            System.out.println("                                                                                                                                   YOU ARE AN IDIOT                                            ");
+            System.out.println("                                                                                                                                                                               YOU ARE AN IDIOT");
+            System.out.println("                                                                                                                                   YOU ARE AN IDIOT                                            ");
+            System.out.println("                                                                                        YOU ARE AN IDIOT                                                                                       ");
+            System.out.println("                                            YOU ARE AN IDIOT                                                                                                                                   ");
+        }
+
+    }
+ }
+
