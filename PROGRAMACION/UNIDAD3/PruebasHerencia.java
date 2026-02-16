cacc0892ef6000933605f8e8be20bd786b5c073f
AlejandroWood <awoorod250@gmail.com>
2026-02-16 12:53:17 +0100

    main

--- PROGRAMACION/UNIDAD3/PruebasHerencia.java
+++ PROGRAMACION/UNIDAD3/PruebasHerencia.java
@@ -2,10 +2,12 @@ package UNIDAD3;
 
 public class PruebasHerencia {
     public static void main(String[] args) {
+        Animal hormiga = new Animal();
         Perro aura = new Perro();
 
         aura.nombre = "Aura";
 
+        hormiga.cagar();
         aura.ladrar();
 
         aura.cagar();
